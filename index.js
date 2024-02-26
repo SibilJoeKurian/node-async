e599b62f6ef59f925ca23fac031c62d0635d0ae1
sibil.kurian@gmail.com <sibil.kurian@gmail.com>
2024-02-25 22:54:26 -0800

    test 6

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ const app = express()
 // Create GET request
 app.get('/one', (req, res) => {
   setTimeout(() => {
-    res.send('First app with 5 milli 5000 delay');
-  }, 5000);
+    res.send('First app with 1000 delay ');
+  }, 1000);
 })
 
 // Create GET request
 app.get('/two', (req, res) => {
   setTimeout(() => {
-    res.send('Second app with 5 milli 8000 delay');
-  }, 8000);
+    res.send('Second app with 2000 delay ');
+  }, 2000);
 })
 
 // Create GET request
 app.get('/three', (req, res) => {
   setTimeout(() => {
-    res.send('Three app with 5 milli Thir delay');
-  }, 10000);
+    res.send('Three app with 3000 delay ');
+  }, 3000);
 })
 
 // Create GET request
 app.get('/four', (req, res) => {
   setTimeout(() => {
-    res.send('Three app with 11000 milli  four  delay');
-  }, 11000);
+    res.send('Three app with 4000 delay');
+  }, 4000);
 })
 
 // Create GET request
 app.get('/five', (req, res) => {
   setTimeout(() => {
-    res.send('Three app with 13 milli Five delay');
-  }, 13000);
+    res.send('Three app with 5000 delay');
+  }, 5000);
 })
 
 // Initialize server
